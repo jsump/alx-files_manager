cd29d12452b9f0f8bc5aee2d27becf2d35c5c89e
jsump <libbyn79@gmail.com>
2024-05-15 02:02:39 +0300

    Task 1 edit

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const MongoClient = require('mongodb');
 
 class DBClient {
     constructor() {
@@ -6,46 +6,38 @@ class DBClient {
         const port = process.env.DB_PORT || 27017;
         const database = process.env.DB_DATABASE || 'files_manager';
 
-        this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
+        const uri = `mongodb://${host}:${port}/${database}`;
+        this.client = new MongoClient(uri, { useUnifiedTopology: true });
         this.dbName = database;
-
-        // Connect to the database when the DBClient instance is created
-        this.connect();
-    }
-
-    async connect() {
-        try {
-            await this.client.connect();
-            console.log("Connected to MongoDB");
-        } catch (error) {
-            console.error("Error connecting to MongoDB:", error);
-            // Throw the error to indicate connection failure
-            throw error;
-        }
     }
 
     async isAlive() {
-        return this.client.isConnected();
+        try {
+            await this.client.connect();
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
 
     async nbUsers() {
         try {
+            await this.client.connect();
             const db = this.client.db(this.dbName);
             const usersCount = await db.collection('users').countDocuments();
             return usersCount;
         } catch (error) {
-            console.error("Error fetching user count:", error);
             return 0;
         }
     }
 
     async nbFiles() {
         try {
+            await this.client.connect();
             const db = this.client.db(this.dbName);
             const filesCount = await db.collection('files').countDocuments();
             return filesCount;
         } catch (error) {
-            console.error("Error fetching file count:", error);
             return 0;
         }
     }
