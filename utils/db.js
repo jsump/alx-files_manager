57b904060c0aa81350ca64b0122f3746fb96b91f
jsump <libbyn79@gmail.com>
2024-05-15 22:25:20 +0300

    Revert "Task 0 connection"
    This reverts commit 49c00671fecfd7c655b0b4a17f94fa5d3774c626.

--- utils/db.js
+++ utils/db.js
@@ -13,6 +13,17 @@ class DBClient {
         this.connect();
     }
 
+    async connect() {
+        try {
+            await this.client.connect();
+            console.log("Connected to MongoDB");
+        } catch (error) {
+            console.error("Error connecting to MongoDB:", error);
+            // Throw the error to indicate connection failure
+            throw error;
+        }
+    }
+
     isAlive() {
         return this.client.isConnected();
     }
